e533fa5f082e941b0d46ceb1717cc4bb6f4f5dc8
RHShoumik <rhshoumik12@gmail.com>
2022-08-30 12:35:05 +0600

    fetch datafrom API and store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-const App = () => {
+const url = "https://restcountries.com/v3.1/all";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsLoading(false);
+      setError(null);
+      console.log(countries);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
+
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <h1>Country App</h1>
+      {countries.map((country) => (
+        <p>{country.name.common}</p>
+      ))}
+    </div>
+  );
 }
-
-export default App
+export default App;
